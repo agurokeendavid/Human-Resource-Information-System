d92d9d3cf288db477bcd44f76fb732d6b751134c
Keen David Aguro <agurokeendavid@gmail.com>
2020-03-17 22:17:27 +0800

    Modified sql file.

--- database/hriscapsu.sql
+++ database/hriscapsu.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 08, 2020 at 04:31 PM
+-- Generation Time: Mar 17, 2020 at 03:16 PM
 -- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.2
+-- PHP Version: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -92,7 +92,9 @@ INSERT INTO `ports` (`port_id`, `port_name`) VALUES
 CREATE TABLE `tbl_leave` (
   `id` int(11) NOT NULL,
   `employee_no` varchar(50) NOT NULL,
-  `type` varchar(20) NOT NULL,
+  `leave_credits` int(5) NOT NULL,
+  `remaining_leave` int(5) NOT NULL,
+  `type` varchar(20) DEFAULT NULL,
   `from_date` date NOT NULL,
   `to_date` date NOT NULL,
   `reason` varchar(150) NOT NULL,
@@ -127,6 +129,28 @@ CREATE TABLE `tbl_list_of_seminars` (
   `number_of_seminars` int(11) DEFAULT NULL,
   `is_deleted` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_positions`
+--
+
+CREATE TABLE `tbl_positions` (
+  `id` int(11) NOT NULL,
+  `position_name` varchar(50) NOT NULL,
+  `position_type` varchar(30) NOT NULL,
+  `salary_grade_level` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_positions`
+--
+
+INSERT INTO `tbl_positions` (`id`, `position_name`, `position_type`, `salary_grade_level`) VALUES
+(1, 'Instructor I', 'Academic', 12),
+(2, 'Instructor II', 'Academic', 13),
+(3, 'Professor II', 'Non - Academic', 24);
 
 -- --------------------------------------------------------
 
@@ -179,6 +203,13 @@ ALTER TABLE `tbl_list_of_seminars`
   ADD UNIQUE KEY `employee_no` (`employee_no`);
 
 --
+-- Indexes for table `tbl_positions`
+--
+ALTER TABLE `tbl_positions`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `position_name` (`position_name`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -192,7 +223,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `employees`
 --
 ALTER TABLE `employees`
-  MODIFY `employee_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `employee_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `ports`
@@ -204,29 +235,25 @@ ALTER TABLE `ports`
 -- AUTO_INCREMENT for table `tbl_leave`
 --
 ALTER TABLE `tbl_leave`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `tbl_list_of_seminars`
 --
 ALTER TABLE `tbl_list_of_seminars`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `tbl_positions`
+--
+ALTER TABLE `tbl_positions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `tbl_leave`
---
-ALTER TABLE `tbl_leave`
-  ADD CONSTRAINT `employee_no_fk` FOREIGN KEY (`employee_no`) REFERENCES `employees` (`employee_no`);
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
